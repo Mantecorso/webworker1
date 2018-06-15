b0612ebb24b90d61c36dd82f472da7496f681d02
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-15 12:37:13 +0200

    boton start worked

--- public/main.js
+++ public/main.js
@@ -11,3 +11,15 @@ worker.addEventListener("message", function (e){
     console.log(otronombre);
     //solo le pedimos los datos del objeto con .data
 });
+
+function startWorker(){
+    var worker = new Worker("./tarea.js");
+    
+    worker.addEventListener("message", function (e){
+        //recibe de tarea el evento
+        var otronombre = JSON.parse(e.data);
+        // parse hace que pasas de string a objeto de javascript
+        console.log(otronombre);
+        //solo le pedimos los datos del objeto con .data
+    });
+}
