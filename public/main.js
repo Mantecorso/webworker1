e861a45a16bff7d80371c47487b7346b6d86dc06
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-15 12:19:53 +0200

    utiliza dos hilos diferentes para trabajar

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ alert();
 
 // crea un nuevo proceso que ejecuta tarea.js
 var worker = new Worker("./tarea.js");
+// Worker es palabra reservada en html5
 
 worker.addEventListener("message", function (e){
     //recibe de tarea el evento
--- public/tarea.js
+++ public/tarea.js
@@ -1,1 +1,14 @@
+//creamos un objeto en JSON y lo enviamos de tarea al main
+var miMensaje = {
+    name: {
+        first: 'Luis',
+        second: 'Jurado'
+    }
+};
+
+postMessage(JSON.stringify(miMensaje));
+
+var tempMessage = JSON.stringify(miMensaje);
+postMessage(tempMessage);
+
 postMessage('es una prueba');
